66f0495270d51f501d9b0c313d3b4fdf9e721389
bendahmanem <mounir.bendahmane@ecole-isitech.fr>
2020-06-27 11:38:20 +0200

    'app.js'

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,17 +4,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi im a react App</h1>
+    //   </div>
+    // );
+    return React.createElement('div', null, 'h1', 'Hi I\'m a react app')
   }
 }
 
